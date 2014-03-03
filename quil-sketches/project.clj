7cba61354efbd644a01dd702920c6fa3bfde2987
Anjenson <anjenson@anjenson.site>
2014-03-03 21:57:04 +0300

    Initial commit: Quil dependency added, files' namespaces corrected.

---
+++ quil-sketches/project.clj
@@ -0,0 +1,9 @@
+(defproject quil-sketches "0.1.0-SNAPSHOT"
+  :description "Examples of using Quil library"
+  :url "https://github.com/quil/quil-examples"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+		  [quil "1.7.0"]]
+;; put the namespace of the example You wish to run after :main in next line 
+)
