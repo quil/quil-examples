96aad42920d8450a9e633f92453cf56aff5e3797
Anjenson <anjenson@anjenson.site>
2014-03-03 22:06:43 +0300

    Cleaned test directory

--- quil-sketches/test/quil_sketches/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns quil-sketches.core-test
-  (:require [clojure.test :refer :all]
-            [quil-sketches.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
