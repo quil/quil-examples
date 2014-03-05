2b1b14bc8eb1958b7618242c52b779ea9467993d
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-03-05 20:57:57 +0000

    Keep project.clj minimal.

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject quil-sketches "0.1.0-SNAPSHOT"
   :description "Examples of using Quil library"
-  :url "https://github.com/quil/quil-examples"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-		  [quil "1.7.0"]])
+                 [quil "1.7.0"]])
