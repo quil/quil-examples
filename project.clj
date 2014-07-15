36069dd655a140f83391567b9cc575ccb1afb390
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-07-15 20:13:12 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject quil-sketches "0.1.0-SNAPSHOT"
   :description "Examples of using Quil library"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.1.0"]])
+                 [quil "2.2.0"]])
