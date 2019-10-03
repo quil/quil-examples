2d1ab195d856d9c3163ae0992d32343f71b5801e
Greg Goltsov <gregory@goltsov.info>
2019-10-03 14:26:51 +0100

    Added project URL to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject quil-sketches "0.1.0-SNAPSHOT"
   :description "Examples of using Quil library"
+  :url "https://github.com/quil/quil-examples"
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "3.0.0"]])
