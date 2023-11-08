03ea6585d9e4c785b3822a11f62b703e82c79912
Charles Comstock <dgtized@gmail.com>
2023-11-07 20:53:10 -0600

    use quil.core :as q for animated-fluffy-clouds

--- src/quil_sketches/gen_art/24_animated_fluffy_clouds.clj
+++ src/quil_sketches/gen_art/24_animated_fluffy_clouds.clj
@@ -1,7 +1,8 @@
 (ns quil-sketches.gen-art.24-animated-fluffy-clouds
-  (:require [quil.core :refer :all]
-            [quil.helpers.seqs :refer [steps seq->stream range-incl tap]]
-            [quil.helpers.calc :refer [mul-add]]))
+  (:require
+   [quil.core :as q]
+   [quil.helpers.calc :refer [mul-add]]
+   [quil.helpers.seqs :refer [range-incl seq->stream steps]]))
 
 ;; Example 24 - Animated Fluffy Clouds
 ;; Taken from Listing 5.3, p89
@@ -52,49 +53,49 @@
 
 (defn draw-point
   [x y noise-factor]
-  (push-matrix)
-  (translate x y)
-  (rotate (* noise-factor (radians 540)))
+  (q/push-matrix)
+  (q/translate x y)
+  (q/rotate (* noise-factor (q/radians 540)))
   (let [edge-size (* noise-factor 35)
         grey (mul-add noise-factor 120 150)
         alph (mul-add noise-factor 120 150)]
-    (no-stroke)
-    (fill grey alph)
-    (ellipse 0 0 edge-size (/ edge-size 2))
-    (pop-matrix)))
+    (q/no-stroke)
+    (q/fill grey alph)
+    (q/ellipse 0 0 edge-size (/ edge-size 2))
+    (q/pop-matrix)))
 
 (defn draw-all-points [x-start y-start]
   (let [step-size 5
-        x-idxs    (range-incl 0 (/ (width) step-size))
-        y-idxs    (range-incl 0 (/ (height) step-size))]
+        x-idxs    (range-incl 0 (/ (q/width) step-size))
+        y-idxs    (range-incl 0 (/ (q/height) step-size))]
     (doseq [x-idx x-idxs
             y-idx y-idxs]
       (let [x       (* step-size x-idx)
             y       (* step-size y-idx)
             x-noise (mul-add x-idx 0.1 x-start)
             y-noise (mul-add y-idx 0.1 y-start)]
-        (draw-point x y (noise x-noise y-noise))))))
+        (draw-point x y (q/noise x-noise y-noise))))))
 
 
 (defn draw []
-  (background 0)
-  (let [starts-str        (state :starts-str)
+  (q/background 0)
+  (let [starts-str        (q/state :starts-str)
         [x-start y-start] (starts-str)]
     (draw-all-points x-start y-start)))
 
 (defn setup []
-  (smooth)
-  (background 0)
-  (frame-rate 24)
+  (q/smooth)
+  (q/background 0)
+  (q/frame-rate 24)
 
-  (let [x-starts (steps (random 10) 0.01)
-        y-starts (steps (random 10) 0.01)
+  (let [x-starts (steps (q/random 10) 0.01)
+        y-starts (steps (q/random 10) 0.01)
         starts   (map list x-starts y-starts)
         starts-str (seq->stream starts)]
 
-    (set-state! :starts-str starts-str)))
+    (q/set-state! :starts-str starts-str)))
 
-(defsketch gen-art-24
+(q/defsketch gen-art-24
   :title "Animated Fluffy Clouds"
   :setup setup
   :draw draw
