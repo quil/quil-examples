0eb91541060ec05f923ae11b4254f7334af80892
Charles Comstock <dgtized@gmail.com>
2023-11-07 20:32:56 -0600

    use quil.core as q for sine-wave-with-noise

--- src/quil_sketches/gen_art/09_sine_wave_with_noise.clj
+++ src/quil_sketches/gen_art/09_sine_wave_with_noise.clj
@@ -1,5 +1,5 @@
 (ns quil-sketches.gen-art.09-sine-wave-with-noise
-  (:require [quil.core :refer :all]
+  (:require [quil.core :as q]
             [quil.helpers.drawing :refer [line-join-points]]
             [quil.helpers.seqs :refer [range-incl]]
             [quil.helpers.calc :refer [mul-add]]))
@@ -35,24 +35,24 @@
 
 
 (defn setup []
-  (background 255)
-  (stroke-weight 5)
-  (smooth)
-  (stroke 0 30)
-  (line 20 50 480 50)
-  (stroke 20 50 70)
+  (q/background 255)
+  (q/stroke-weight 5)
+  (q/smooth)
+  (q/stroke 0 30)
+  (q/line 20 50 480 50)
+  (q/stroke 20 50 70)
 
   (let [xs        (range-incl 20 480 1)
-        rads      (map radians (range))
-        ys        (map sin rads)
-        ys        (map #(pow % 3) ys)
-        ys        (map (fn [y rad] (* 30 y (noise (* 2 rad)))) ys rads)
+        rads      (map q/radians (range))
+        ys        (map q/sin rads)
+        ys        (map #(q/pow % 3) ys)
+        ys        (map (fn [y rad] (* 30 y (q/noise (* 2 rad)))) ys rads)
         scaled-ys (mul-add ys 1 50)
 
         line-args (line-join-points xs scaled-ys)]
-    (dorun (map #(apply line %) line-args))))
+    (dorun (map #(apply q/line %) line-args))))
 
-(defsketch gen-art-9
+(q/defsketch gen-art-9
   :title "Sine Wave with Noise"
   :setup setup
   :size [500 100])
