a9b1adf4b457377887dd988475333ccc8e0d39fa
Charles Comstock <dgtized@gmail.com>
2023-11-07 20:35:49 -0600

    use quil.core :as q for aspiral example

--- src/quil_sketches/gen_art/12_spiral.clj
+++ src/quil_sketches/gen_art/12_spiral.clj
@@ -1,5 +1,5 @@
 (ns quil-sketches.gen-art.12-aspiral
-  (:require [quil.core :refer :all]
+  (:require [quil.core :as q]
             [quil.helpers.drawing :refer [line-join-points]]
             [quil.helpers.seqs :refer [range-incl steps]]))
 
@@ -36,24 +36,24 @@
 ;; }
 
 (defn setup []
-  (background 255)
-  (stroke-weight 5)
-  (smooth)
+  (q/background 255)
+  (q/stroke-weight 5)
+  (q/smooth)
   (let [radius    100
         cent-x    250
         cent-y    150
-        radians   (map radians (range-incl 0 1440 5))
+        radians   (map q/radians (range-incl 0 1440 5))
         radii     (steps 10 0.5)
-        xs        (map (fn [radians radius] (+ cent-x (* radius (cos radians)))) radians radii)
-        ys        (map (fn [radians radius] (+ cent-y (* radius (sin radians)))) radians radii)
+        xs        (map (fn [radians radius] (+ cent-x (* radius (q/cos radians)))) radians radii)
+        ys        (map (fn [radians radius] (+ cent-y (* radius (q/sin radians)))) radians radii)
         line-args (line-join-points xs ys)]
-    (stroke 0 30)
-    (no-fill)
-    (ellipse cent-x cent-y (* radius 2) (* radius 2))
-    (stroke 20 50 70)
-    (dorun (map #(apply line %) line-args))))
+    (q/stroke 0 30)
+    (q/no-fill)
+    (q/ellipse cent-x cent-y (* radius 2) (* radius 2))
+    (q/stroke 20 50 70)
+    (dorun (map #(apply q/line %) line-args))))
 
-(defsketch gen-art-12
+(q/defsketch gen-art-12
   :title "Spiral"
   :setup setup
   :size [500 300])
