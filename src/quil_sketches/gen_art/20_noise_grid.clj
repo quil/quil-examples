4cc5ba6d6a91fc8b219231df91d764fb796da09c
Charles Comstock <dgtized@gmail.com>
2023-11-07 20:47:00 -0600

    use quil.core as q for noise-grid example

--- src/quil_sketches/gen_art/20_noise_grid.clj
+++ src/quil_sketches/gen_art/20_noise_grid.clj
@@ -1,7 +1,8 @@
 (ns quil-sketches.gen-art.20-noise-grid
-  (:require [quil.core :refer :all]
-            [quil.helpers.seqs :refer [range-incl]]
-            [quil.helpers.calc :refer [mul-add]]))
+  (:require
+   [quil.core :as q]
+   [quil.helpers.calc :refer [mul-add]]
+   [quil.helpers.seqs :refer [range-incl]]))
 
 ;; Example 20 - 2D Noise Grid
 ;; Taken from Listing 5.1, p84
@@ -26,20 +27,20 @@
 ;; }
 
 (defn setup []
-  (smooth)
-  (background 255)
+  (q/smooth)
+  (q/background 255)
   (dorun
-   (let [x-start (random 10)
-         y-start (random 10)]
-     (for [y (range-incl (height))
-           x (range-incl (width))]
+   (let [x-start (q/random 10)
+         y-start (q/random 10)]
+     (for [y (range-incl (q/height))
+           x (range-incl (q/width))]
        (let [x-noise (mul-add x 0.01 x-start)
              y-noise (mul-add y 0.01 y-start)
-             alph    (* 255 (noise x-noise y-noise))]
-         (stroke 0 alph)
-         (line x y (inc x) (inc y)))))))
+             alph    (* 255 (q/noise x-noise y-noise))]
+         (q/stroke 0 alph)
+         (q/line x y (inc x) (inc y)))))))
 
-(defsketch gen-art-20
+(q/defsketch gen-art-20
   :title "2D Noise Grid"
   :setup setup
   :size [300 300])
